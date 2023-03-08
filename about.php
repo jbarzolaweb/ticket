e1bafca11cd37c9ab4a72b3ced49867b1e4a33ff
jbarzolaweb <barzola.jose@gmail.com>
2023-03-08 10:17:42 -0500

    commit 2

---
+++ about.php
@@ -0,0 +1,43 @@
+<?php
+    $title ="Sobre Mi | ";
+    include "head.php";
+    include "sidebar.php";  
+?>
+
+    <div class="right_col" role="main"> <!-- page content -->
+        <div class="">
+            <div class="page-title">
+            <br>
+                <div class="row">
+            	    <div class="col-md-6 col-md-offset-3">
+                        <h1>Acerca del software</h1>
+                        
+                        <br><br>
+
+                        <h2>TICKET - <small>Sistema de Tickets de Soporte</small></h2>
+                        <p>Sistema de Tickets de soporte de proposito general</p>
+
+                        <h1 style="text-align: center;">TICKET</h1>
+                        <h3>Descripcion</h3>
+                        <p>sistema para gestionar tickets de soporte usando proyectos, categorias, prioridades y mas, ideal para todo tipo de negocios, desarrollo de proyectos, gestion personal, etc, el sistema esta desarrollado con PHP y MySQL.</p>
+                        <p>sistema de tickets de soporte en el que puedes rellenar tu perfil, agregar ticket, proyectos y categorias, generar reportes y mucho mas.</p>
+
+                       
+                        <h3>Modulos</h3>
+                        <p>Defino a grandes rasgos los modulos generales del sistema</p>
+                        <ul>
+                        <li><b>Usuarios</b>: Se puede agregar editar y eliminar usuarios.</li>
+                        <li><b>Tickets</b>: Cada usuario puede agregar un ticket.</li>
+                        <li><b>Proyectos</b>: Cada usuario puede agregar los proyectos que desee.</li>
+                        <li><b>Perfiles</b>: Los usuarios pueden rellenar su perfil</li>
+                        <li><b>Categorias</b>: Los usuarios agregar categorias.</li>
+                        <li><b>Reportes</b>: Los usuarios pueden generar reportes.</li>
+                        </ul>
+                  
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div><!-- /page content -->
+
+<?php include "footer.php" ?>
